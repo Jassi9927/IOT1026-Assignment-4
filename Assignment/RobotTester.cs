95e2353085f12639547687360c317089eddd6908
Jassi9927 <123195722+Jassi9927@users.noreply.github.com>
2023-06-13 09:37:44 -0400

    Update RobotTester.cs

--- Assignment/RobotTester.cs
+++ Assignment/RobotTester.cs
@@ -1,43 +1,45 @@
-﻿using Assignment.InterfaceCommand;
+//A00274134
+using Assignment.InterfaceCommand;
 
-namespace Assignment;
-
-static class RobotTester
+namespace Assignment
 {
-    public static void TestRobot()
+    static class RobotTester
     {
-        int totalCommands = 1;
-        Robot robot = new Robot();
-        Console.WriteLine("Choose 6 Commands : \nON\nOFF\nNORTH\nSOUTH\nEAST\nSOUTH\nWEST\nREBOOT\n");
-        do
+        public static void TestRobot()
         {
-            Console.Write($"Choose {totalCommands} command : ");
-            string? con = Console.ReadLine()?.ToUpper();
-            RobotCommand? command = com switch
+            int totalCommands = 1;
+            Robot robot = new Robot();
+            Console.WriteLine("Choose 6 Commands : \nON\nOFF\nNORTH\nSOUTH\nEAST\nSOUTH\nWEST\nREBOOT\n");
+            do
             {
-                "ON" => new OnCommand(),
-                "OFF" => new OffCommand(),
-                "NORTH" => new NorthCommand(),
-                "SOUTH" => new SouthCommand(),
-                "EAST" => new EastCommand(),
-                "WEST" => new WestCommand(),
-                "REBOOT" => new RebootCommand(),
-                _ => null
-            };
-            if (command != null)
-            {
-                robot.LoadCommand(command);
-                totalCommands++;
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("------- Please enter valid commands only --------");
-                Console.ResetColor();
-            }
-        } while (totalCommands <= 6);
-        Console.WriteLine();
-        robot.Run();
-        Console.ReadLine();
+                Console.Write($"Choose {totalCommands} command : ");
+                string? com = Console.ReadLine()?.ToUpper();
+                RobotCommand? command = com switch
+                {
+                    "ON" => new OnCommand(),
+                    "OFF" => new OffCommand(),
+                    "NORTH" => new NorthCommand(),
+                    "SOUTH" => new SouthCommand(),
+                    "EAST" => new EastCommand(),
+                    "WEST" => new WestCommand(),
+                    "REBOOT" => new RebootCommand(),
+                    _ => null
+                };
+                if (command != null)
+                {
+                    robot.LoadCommand(command);
+                    totalCommands++;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("------- Please enter valid commands only --------");
+                    Console.ResetColor();
+                }
+            } while (totalCommands <= 6);
+            Console.WriteLine();
+            robot.Run();
+            Console.ReadLine();
+        }
     }
 }
